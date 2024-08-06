1ea646f2b6f1107f96c06704a7137c2a5eb88fa2
fabiano <fabiano@uam.es>
2024-08-06 16:51:23 +0200

    adding a new file

---
+++ script_all.m
@@ -0,0 +1,132 @@
+saveListOfOpenFiles('BaroniFulcher2024');
+openFilesInList('BaroniFulcher2024');
+openFilesInTxtList('BaroniFulcher2024');
+
+%%%% SYNTH SPIKE TRAINS %%%%
+
+% single-scale
+
+script_sin_train_range
+clear all
+close all
+script_sinseq_train_range
+clear all
+close all
+
+% dual-scale
+
+script_G_train_range
+clear all
+close all
+script_Gseq_train_range
+clear all
+close all
+
+
+
+% sweeping sim_time and seed
+
+script_sin_train_win_range
+clear all
+close all
+
+script_sinseq_train_win_range
+clear all
+close all
+
+script_G_train_win_range
+clear all
+close all
+
+script_Gseq_train_win_range
+clear all
+close all
+
+
+
+% sweeping n_neu and seed
+
+script_sin_train_nneu_range
+clear all
+close all
+
+script_sinseq_train_nneu_range
+clear all
+close all
+
+script_G_train_nneu_range
+clear all
+close all
+
+script_Gseq_train_nneu_range
+clear all
+close all
+
+
+
+% analyzing win sims from all synth train families together
+script_all_train_win_range
+clear all
+close all
+
+% analyzing nneu sims from all synth train families together
+script_all_train_nneu_range
+clear all
+close all
+
+
+
+
+
+
+
+
+
+% analyzing all par sweep single-scale synth train families together
+script_all_train_range_ss
+clear all
+close all
+
+% analyzing all par sweep dual-scale synth train families together
+script_all_train_range_ds
+clear all
+close all
+
+% analyzing all par sweep synth train families together
+script_all_train_range
+clear all
+close all
+
+
+
+
+
+
+
+%%%% DATA %%%%
+
+script_data_train_See18
+clear all
+close all
+
+script_data_train_Smith08
+clear all
+close all
+
+script_data_train_Form22
+clear all
+close all
+
+
+
+
+
+% analyzing all par sweep synth train families together + data
+script_all_train_range_data
+clear all
+close all
+
+
+
+
+
